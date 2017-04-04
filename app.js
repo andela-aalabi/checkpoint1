1c078bb8195d0b274690499c4623cb5f82f34739
Adeyinka Alabi <adeyinka.alabi@andela.com>
2017-04-04 21:09:02 +0100

    add modals

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-/* global angular, InvertedIndex, document*/
+/* global angular, InvertedIndex, document, toastr*/
 const invApp = angular.module('invIndex', []);
+
+toastr.options.closeDuration = 500;
+toastr.options.closeButton = true;
+toastr.options.preventDuplicates = true;
+toastr.options.closeMethod = 'fadeOut';
 
 invApp.controller('invController', ['$scope', ($scope) => {
   const scope = $scope;
@@ -15,12 +20,16 @@ invApp.controller('invController', ['$scope', ($scope) => {
     for (let i = 0; i < doc.files.length; i += 1) {
       invIndex.readFile(doc.files[i]).then((fileContent) => {
         if (scope.validateJson(fileContent)) {
-          scope.filesRead[doc.files[i].name] = fileContent;
-          if (!(scope.uploadedFiles.includes(doc.files[i].name))) {
-            scope.$apply(scope.uploadedFiles.push(doc.files[i].name));
+          if (invIndex.validateFile(JSON.parse(fileContent))) {
+            scope.filesRead[doc.files[i].name] = fileContent;
+            if (!(scope.uploadedFiles.includes(doc.files[i].name))) {
+              scope.$apply(scope.uploadedFiles.push(doc.files[i].name));
+            }
+          } else {
+            toastr.error('invalide file format');
           }
         } else {
-          console.log(`${doc.files[i].name} is not json`);
+          toastr.error(`${doc.files[i].name} is not json`);
         }
       });
     }
@@ -35,12 +44,10 @@ invApp.controller('invController', ['$scope', ($scope) => {
     }
   };
 
-  document.getElementById('upload').addEventListener('change', scope.readFile);
-
   scope.getIndex = (filename) => {
-    const titles = [];
-    const file = JSON.parse(scope.filesRead[filename]);
-    if (invIndex.validateFile(file)) {
+    try {
+      const titles = [];
+      const file = JSON.parse(scope.filesRead[filename]);
       file.forEach(obj => titles.push(obj.title));
       const indices = invIndex.getIndex(file, filename);
       const documents = [];
@@ -57,44 +64,54 @@ invApp.controller('invController', ['$scope', ($scope) => {
       ];
       scope.count += 1;
       scope.filesIndexed[filename] = scope.indexed;
-    } else {
-      console.log('invalide file format');
+    } catch (e) {
+      toastr.info('Please select a file to get indexed');
     }
   };
 
   const searchIndexOneFile = () => {
-    const cleanedTerms = invIndex.tokenize(scope.selectedSearch);
-    const result = {};
-    cleanedTerms.forEach((term) => {
-      const found = invIndex.searchIndex(term, scope.fileSearch);
-      if (found !== false) {
-        result[term] = found;
+    try {
+      if (!scope.fileSearch) {
+        throw new Error('Please select a file to search');
       }
-    });
+      if (!scope.selectedSearch) {
+        throw new Error('Please type in word(s) to search for');
+      }
+      const cleanedTerms = invIndex.tokenize(scope.selectedSearch);
+      if (cleanedTerms[0] === '' && cleanedTerms.length === 1) {
+        throw new Error('Please type word(s) to search for and not symbols');
+      } else {
+        const result = {};
+        scope.selectedSearch = cleanedTerms.toString();
+        cleanedTerms.forEach((term) => {
+          const found = invIndex.searchIndex(term, scope.fileSearch);
+          result[term] = found;
+        });
 
-    const titles = [];
-    const file = JSON.parse(scope.filesRead[scope.fileSearch]);
-    file.forEach(obj => titles.push(obj.title));
-    const documents = [];
-    for (let i = 0; i < titles.length; i += 1) {
-      documents.push(i);
+        const titles = [];
+        const file = JSON.parse(scope.filesRead[scope.fileSearch]);
+        file.forEach(obj => titles.push(obj.title));
+        const documents = [];
+        for (let i = 0; i < titles.length; i += 1) {
+          documents.push(i);
+        }
+        scope.search.push({
+          indexes: result,
+          docs: documents,
+          searchedFile: scope.fileSearch,
+          title: titles,
+        });
+        scope.showIndex = false;
+      }
+    } catch (e) {
+      toastr.error(e);
     }
-    scope.search.push({
-      indexes: result,
-      docs: documents,
-      searchedFile: scope.fileSearch,
-      title: titles,
-    });
-    console.log(scope.search[0].indexes);
-
-    scope.showIndex = false;
   };
 
   scope.searchIndex = () => {
     scope.search = [];
     if (scope.fileToSearch === 'All') {
       const all = Object.keys(scope.filesIndexed);
-      console.log(all);
       all.forEach((file) => {
         scope.fileSearch = file;
         searchIndexOneFile();
@@ -104,4 +121,6 @@ invApp.controller('invController', ['$scope', ($scope) => {
       searchIndexOneFile();
     }
   };
+
+  document.getElementById('upload').addEventListener('change', scope.readFile);
 }]);
