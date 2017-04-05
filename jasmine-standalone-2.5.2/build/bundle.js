02bb10c7849581db254f0124fc751d3f4d1dd5f7
Adeyinka Alabi <adeyinka.alabi@andela.com>
2017-04-05 04:45:09 +0100

    add some files for testing and travis-ci config

--- jasmine-standalone-2.5.2/build/bundle.js
+++ jasmine-standalone-2.5.2/build/bundle.js
@@ -1,30 +1,24 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* global describe, it, expect */
 
-const valid = require('../../valid1.json');
+const valid1 = require('../../valid1.json');
 
 const invIndex = new InvertedIndex();
 
-const testBook = [{
-  title: 'Checkpoint one',
-  text: 'Testing getIndex function',
-},
+describe('InvertedIndex', () => {
 
-{
-  title: 'Checkpoint Two',
-  text: 'Coming soon',
-},
-];
-
-describe('InvertedIndex : define methods', () => {
-  invIndex.createIndex(testBook, valid);
-  it('Should ensure that invertedIndex methods are defined', () => {
-    expect(invIndex.getIndex).toBeDefined();
-    expect(invIndex.createIndex).toBeDefined();
-    expect(invIndex.tokenize).toBeDefined();
-    expect(invIndex.validateFile).toBeDefined();
-    expect(invIndex.searchIndex).toBeDefined();
+  describe('defined methods', () => {
+    invIndex.createIndex(valid1, 'valid1.json');
+    it('Should ensure that invertedIndex methods are defined', () => {
+      expect(invIndex.getIndex).not.toBeUndefined();
+      expect(invIndex.createIndex).not.toBeUndefined();
+      expect(invIndex.tokenize).not.toBeUndefined();
+      expect(invIndex.validateFile).not.toBeUndefined();
+      expect(invIndex.searchIndex).not.toBeUndefined();
+    });
   });
+
+
 });
 
 },{"../../valid1.json":2}],2:[function(require,module,exports){
--- jasmine-standalone-2.5.2/spec/inverted-index-spec.js
+++ jasmine-standalone-2.5.2/spec/inverted-index-spec.js
@@ -1,27 +1,65 @@
 /* global describe, it, expect */
 
-const valid = require('../../valid1.json');
+const valid1 = require('../../valid1.json');
+const valid2 = require('../../valid2.json');
+const valid3 = require('../../valid3.json');
+const invalid1 = require('../../invalid1.json');
+const invalid2 = require('../../invalid2.json');
+const invalid3 = require('../../invalid3.json');
+const invalid4 = require('../../invalid4.json');
+const invalid5 = require('../../invalid5.json');
+const invalid6 = require('../../invalid6.json');
 
 const invIndex = new InvertedIndex();
 
-const testBook = [{
-  title: 'Checkpoint one',
-  text: 'Testing getIndex function',
-},
+describe('InvertedIndex', () => {
 
-{
-  title: 'Checkpoint Two',
-  text: 'Coming soon',
-},
-];
-
-describe('InvertedIndex : define methods', () => {
-  invIndex.createIndex(testBook, valid);
-  it('Should ensure that invertedIndex methods are defined', () => {
-    expect(invIndex.getIndex).toBeDefined();
-    expect(invIndex.createIndex).toBeDefined();
-    expect(invIndex.tokenize).toBeDefined();
-    expect(invIndex.validateFile).toBeDefined();
-    expect(invIndex.searchIndex).toBeDefined();
+  describe('defined methods', () => {
+    invIndex.createIndex(valid1, 'valid1.json');
+    it('Should ensure that invertedIndex methods are defined', () => {
+      expect(invIndex.getIndex).not.toBeUndefined();
+      expect(invIndex.createIndex).not.toBeUndefined();
+      expect(invIndex.tokenize).not.toBeUndefined();
+      expect(invIndex.validateFile).not.toBeUndefined();
+      expect(invIndex.searchIndex).not.toBeUndefined();
+    });
   });
+
+  describe('class', () => {
+    it('should have a readFile method', () => {
+      expect(typeof InvertedIndex.readFile).toBe('function');
+    });
+
+    it('should have a createIndex method', () => {
+      expect(typeof invertedIndex.createIndex).toBe('function');
+    });
+
+    it('should have a getIndex method', () => {
+      expect(typeof invertedIndex.getIndex).toBe('function');
+    });
+
+    it('should have a searchIndex method', () => {
+      expect(typeof invertedIndex.searchIndex).toBe('function');
+    });
+
+    it('should have a validateFile method', () => {
+      expect(typeof InvertedIndex.validateFile).toBe('function');
+    });
+
+    it('should have a tokenize method', () => {
+      expect(typeof InvertedIndex.tokenize).toBe('function');
+    });
+  });
+
+  describe('createIndex', () => {
+    invIndex.createIndex(valid1, 'valid1.json');
+    it('Should ensure that invertedIndex methods are defined', () => {
+      expect(invIndex.getIndex).not.toBeUndefined();
+      expect(invIndex.createIndex).not.toBeUndefined();
+      expect(invIndex.tokenize).not.toBeUndefined();
+      expect(invIndex.validateFile).not.toBeUndefined();
+      expect(invIndex.searchIndex).not.toBeUndefined();
+    });
+  });
+
 });
